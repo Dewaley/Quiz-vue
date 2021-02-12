e448033c37eeed732ca1c59612a05014529329ec
Dewaley <adewaleademilua@gmail.com>
2021-02-12 05:17:46 +0100

    API

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,48 @@
 <template>
   <div id="app" class="font-nunito overflow-hidden">
     <Header />
-    <QuestionContainer />
+    <QuestionContainer
+      :currentQuestion="questions[index]"
+      :next="next"
+      v-if="questions.length"
+    />
   </div>
 </template>
 
 <script>
 import Header from './components/Header'
 import QuestionContainer from './components/QuestionContainer'
+import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+Vue.use(VueAxios, axios)
 
 export default {
   name: 'App',
   components: {
     Header,
     QuestionContainer,
+  },
+  data() {
+    return {
+      questions: [],
+      index: 0,
+    }
+  },
+  methods: {
+    next() {
+      this.index++
+    },
+  },
+  mounted() {
+    Vue.axios
+      .get(
+        'https://opentdb.com/api.php?amount=10&category=19&difficulty=medium&type=multiple'
+      )
+      .then((res) => {
+        this.questions = res.data.results
+        console.log(this.questions)
+      })
   },
 }
 </script>
--- src/components/QuestionContainer.vue
+++ src/components/QuestionContainer.vue
@@ -1,53 +1,56 @@
 <template>
   <div class="w-screen flex justify-center items-center">
-    <div class="text-center w-2/5 bg-gray-300 p-5 md:w-2/4 sm:w-">
+    <div
+      class="text-center w-2/5 bg-gray-300 p-5 md:w-9/12 lg:w-7/12 sm:w-screen"
+    >
       <div class="p-1 border-b-2 text-xl">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto qui
-        blanditiis adipisci hic possimus assumenda molestiae tempora nisi
-        voluptates voluptatem?
+        {{ currentQuestion.question }}
       </div>
       <div class="p-4">
-        <div class="bg-white p-2 border border-gray-300">
+        <div
+          class="bg-white p-2 border border-gray-300 cursor-pointer hover:bg-gray-400"
+        >
           I want a dog
         </div>
-        <div class="bg-white p-2 border border-gray-300">
+        <div
+          class="bg-white p-2 border border-gray-300 cursor-pointer hover:bg-gray-400"
+        >
           I want a dog
         </div>
-        <div class="bg-white p-2 border border-gray-300">
+        <div
+          class="bg-white p-2 border border-gray-300 cursor-pointer hover:bg-gray-400"
+        >
           I want a dog
         </div>
-        <div class="bg-white p-2 border border-gray-300">
+        <div
+          class="bg-white p-2 border border-gray-300 cursor-pointer hover:bg-gray-400"
+        >
           I want a dog
         </div>
       </div>
       <div>
-        <button class="text-white bg-indigo-400 p-1 rounded focus:outline-none mx-1">Submit</button>
-        <button class="bg-green-500 text-white p-1 rounded focus:outline-none mx-1">Next</button>
+        <button
+          class="text-white bg-indigo-400 p-1 rounded focus:outline-none mx-1"
+        >
+          Submit
+        </button>
+        <button
+          class="bg-green-500 text-white p-1 rounded focus:outline-none mx-1"
+          @click="next"
+        >
+          Next
+        </button>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import Vue from 'vue'
-import axios from 'axios'
-import VueAxios from 'vue-axios'
-Vue.use(VueAxios,axios)
-
 export default {
   name: 'QuestionContainer',
-  props: {},
-  data() {
-   return {
-    questions: []
-   }
-  },
-  mounted: function() {
-   Vue.axios.get('https://opentdb.com/api.php?amount=10&category=19&difficulty=medium&type=multiple')
-   .then((res)=> {
-    this.questions=res.data.results
-    console.log(this.questions);
-   })
+  props: {
+    currentQuestion: Object,
+    next: Function
   }
 }
 </script>
